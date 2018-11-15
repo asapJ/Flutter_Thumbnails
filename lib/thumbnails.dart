ef7e36ea1031aaea2e5d0433bc01da1d2fb31d27
ASAP JAY <asapjayfroshcr9@gmail.com>
2018-11-15 14:49:29 +0100

    few changes

--- lib/thumbnails.dart
+++ lib/thumbnails.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
-import 'package:meta/meta.dart';
 import 'package:flutter/services.dart';
 import 'package:thumbnails/validators.dart';
 
